77eb596324fa2bfa4c9e39d0ff383bed9ecec5ea
gab2135 <120064219+gab2135@users.noreply.github.com>
2023-03-27 18:45:29 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Clase-Quiz
+# PROC37-1_4-plantilla-proyecto
+## Plantilla del proyecto para la clase 37 nivel PRO 1:4.
+Creación de un formulario y depuración de errores.
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+// Llama al método start() dentro de la clase quiz
+
+}
+
+
+function draw(){
+  background("pink");
+
+}
